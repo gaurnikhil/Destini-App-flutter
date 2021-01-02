c12deac35ab8bb98cc980c16d263c28d805b4194
Nikhil Gaur <ngaur6834@gmail.com>
2021-01-02 16:47:48 +0530

    a destini story completion app

--- lib/story.dart
+++ lib/story.dart
@@ -1,3 +1,16 @@
+import 'story_brain.dart';
+
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story(String t, String c1, String c2) {
+    storyTitle = t;
+    choice1 = c1;
+    choice2 = c2;
+  }
+}
 //TODO: Step 2 - Create a new class called Story.
 
 //TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
